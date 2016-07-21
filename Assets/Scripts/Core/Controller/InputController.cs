2035a03291bb635ef4d58cdd0f5648224d21497d
Dani Santeugini <hefesto84@gmail.com>
2016-07-21 21:52:00 +0200

    Basic raycast implemented

---
+++ Assets/Scripts/Core/Controller/InputController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hit;
+
+		if (Physics.Raycast (ray, out hit, 100)) {
+			Debug.DrawLine (ray.origin, hit.point);
+			print(hit.collider.name);
+		}
+	}
+}
