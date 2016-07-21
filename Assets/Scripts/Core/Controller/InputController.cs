abaf3c2bfbfb4dcb032c559bd896728a39c00115
Dani Santeugini <hefesto84@gmail.com>
2016-07-21 22:03:19 +0200

    Basic isometric camera implemented

--- Assets/Scripts/Core/Controller/InputController.cs
+++ Assets/Scripts/Core/Controller/InputController.cs
@@ -10,12 +10,20 @@ public class InputController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if (Input.GetMouseButton (0)) {
+			sendRayCast ();
+		}
+	}
+
+	private void sendRayCast(){
 		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 		RaycastHit hit;
 
 		if (Physics.Raycast (ray, out hit, 100)) {
 			Debug.DrawLine (ray.origin, hit.point);
-			print(hit.collider.name);
+			GameObject go = hit.collider.transform.gameObject;
+			print (go.name);
+			go.SetActive (false);
 		}
 	}
 }
